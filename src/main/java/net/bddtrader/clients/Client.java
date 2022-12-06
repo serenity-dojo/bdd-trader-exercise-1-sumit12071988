abdee3e2902e55ce16f4f8f0ce93e87855049c65
sumit12071988 <sumit786.88@gmail.com>
2022-12-06 13:08:48 +0800

    Making post calls

--- src/main/java/net/bddtrader/clients/Client.java
+++ src/main/java/net/bddtrader/clients/Client.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Client {
-
+// ---------------------------------------------------------------------------------------------------------------------
+    // Step 1: Create Private variables for KEYS
     private final Long id;
     private final String firstName;
     private final String lastName;
     private final String email;
-
+// ---------------------------------------------------------------------------------------------------------------------
+    // Step 2: Create constructor to act as SETTERS
     @JsonCreator
     public Client(@JsonProperty("id") Long id,
                   @JsonProperty("firstName") String firstName,
@@ -20,7 +22,8 @@ public class Client {
         this.lastName = lastName;
         this.email = email;
     }
-
+// ---------------------------------------------------------------------------------------------------------------------
+    // Step 3: Create Public GETTERS
     public Long getId() {
         return id;
     }
@@ -36,28 +39,36 @@ public class Client {
     public String getEmail() {
         return email;
     }
-
+// ------- INTERFACES --------------------------------------------------------------------------------------------------
     public static AndLastName withFirstName(String firstName) {
-        return new ClientBuilder(firstName);
+        return new ClientBuilder(firstName);    // returning object of ClientBuilder class for method chaining
     }
 
-    public interface AndLastName { AndEmail andLastName(String lastName); }
-    public interface AndEmail { Client andEmail(String email); }
-
+    public interface AndLastName {
+        AndEmail andLastName(String lastName);  // abstract method
+    }
+    public interface AndEmail {
+        Client andEmail(String email);  // abstract method
+    }
+// ---------------------------------------------------------------------------------------------------------------------
 
     public static class ClientBuilder implements AndLastName, AndEmail {
         private final String firstName;
         private String lastName;
 
+        // Constructor - SETTER
         public ClientBuilder(String firstName) {
             this.firstName = firstName;
         }
 
+        // Public SETTER
+        // Return type is object for method chaining
         public AndEmail andLastName(String lastName) {
             this.lastName = lastName;
             return this;
         }
 
+        // Final Key in JSON is Email, thus return type is the object of main class
         public Client andEmail(String email) {
             return new Client(null, firstName, lastName, email);
         }
--- src/test/java/net/bddtrader/F_POSTCall.java
+++ src/test/java/net/bddtrader/F_POSTCall.java
@@ -2,8 +2,13 @@ package net.bddtrader;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import net.bddtrader.clients.Client;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import static net.serenitybdd.rest.RestRequests.given;
 import static org.hamcrest.Matchers.*;
@@ -16,7 +21,7 @@ public class F_POSTCall {
     }
 
     @Test
-    public void each_new_client_should_get_a_unique_id(){
+    public void post_with_JSONBody_as_String(){
         String jsonBody= """
                             {
                                 "email": "scott_atkins@gmail.com",
@@ -31,6 +36,27 @@ public class F_POSTCall {
                 .accept(ContentType.ANY)
                 .body(jsonBody)
         .when()
+                .log().all()
+                .post()
+        .then()
+                .statusCode(200)
+                .body("id",not(equalTo(0)))
+                .body("email",equalTo("scott_atkins@gmail.com"))
+                .body("firstName",equalTo("Scott"))
+                .body("lastName",equalTo("Atkins"));
+    }
+
+    @Test
+    public void post_with_JSONBody_as_File(){
+        File fileObject = new File("src/test/resources/client.json");
+
+        given()
+                .basePath("/api/client")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.ANY)
+                .body(fileObject)
+        .when()
+                .log().all()
                 .post()
         .then()
                 .log().all()
@@ -40,4 +66,53 @@ public class F_POSTCall {
                 .body("firstName",equalTo("Scott"))
                 .body("lastName",equalTo("Atkins"));
     }
+
+@Test
+    public void post_with_JSONBody_as_POJO(){
+
+    Client jsonClassObject = Client.withFirstName("Sumit")
+                                .andLastName("Saha")
+                                .andEmail("sumit.saha@gmail.com");
+
+    given()
+            .basePath("/api/client")
+            .contentType(ContentType.JSON)
+            .accept(ContentType.ANY)
+            .body(jsonClassObject)
+    .when()
+            .post()
+    .then()
+            .log().all()
+            .statusCode(200)
+            .body("id",not(equalTo(0)))
+            .body("email",equalTo("sumit.saha@gmail.com"))
+            .body("firstName",equalTo("Sumit"))
+            .body("lastName",equalTo("Saha"));
+    }
+
+    @Test
+    public void post_with_JSONBody_as_Map(){
+        Map<String,Object> jsonMapObject = new HashMap<>();
+        jsonMapObject.put("firstName","Sumit");
+        jsonMapObject.put("lastName","Saha");
+        jsonMapObject.put("email","sumit.saha@gmail.com");
+        jsonMapObject.put("address","Texas");   // Creating a Key which is not required in JSON Body using maps will not affect the outcome
+                                                //  This KEY in this case will be ignored and only required keys will be passed by Rest Assured
+
+        given()
+                .basePath("/api/client")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.ANY)
+                .body(jsonMapObject)
+        .when()
+                .log().all()
+                .post()
+        .then()
+                .statusCode(200)
+                .body("id",not(equalTo(0)))
+                .body("email",equalTo("sumit.saha@gmail.com"))
+                .body("firstName",equalTo("Sumit"))
+                .body("lastName",equalTo("Saha"));
+    }
+
 }
